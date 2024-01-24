09410a1fe732a0279db6d8d3d4c55dea3fc72d78
BHAMHASTRA <155980780+BHAMHASTRA@users.noreply.github.com>
2024-01-24 12:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,19 +23,19 @@ SESSION3 = getenv("SESSION3")
 SESSION4 = getenv("SESSION4")
 SESSION5 = getenv("SESSION5")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/gamingbuddyyy/weedleaf-ub")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PRADHAN474")
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-EXTRA_IMG = getenv("EXTRA_IMG", "https://graph.org/file/6b1e6a6b8b0bb4934689f.jpg")
+EXTRA_IMG = getenv("EXTRA_IMG", "https://te.legra.ph/file/bfd4abee19f36133df12d.jpg")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1918848221").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6938422477").split()))
 
 for y in OWNER_ID:
     SUDO_USERS.append(y)
